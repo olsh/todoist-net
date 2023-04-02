522efdf1a5391ee02650922215a6e39341a23d26
Oleg <olsh.me@gmail.com>
2023-04-02 18:19:48 +0300

    Add IsFullDay parameter for text due date

--- src/Todoist.Net/Models/DueDate.cs
+++ src/Todoist.Net/Models/DueDate.cs
@@ -30,12 +30,14 @@ namespace Todoist.Net.Models
         /// </summary>
         /// <param name="text">The text (every day; each monday)</param>
         /// <param name="date">The date time. this can be used with 'text' parameter to create a recurring from specific date</param>
+        /// <param name="isFullDay">if set to <c>true</c> then it's a full day event.</param>
         /// <param name="timezone">The timezone.</param>
         /// <param name="language">The language.</param>
-        public DueDate(string text, DateTime? date = null, string timezone = null, Language language = null)
+        public DueDate(string text, DateTime? date = null, bool isFullDay = false, string timezone = null, Language language = null)
         {
             Text = text;
             Date = date;
+            IsFullDay = isFullDay;
             Timezone = timezone;
             Language = language;
         }
