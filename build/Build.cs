9b3db2cd1387fc29a91ef5f9656157e64699c777
Oleg Shevchenko <olsh.me@gmail.com>
2023-09-01 01:37:11 +0300

    Fix SonarQube analysis

--- build/Build.cs
+++ build/Build.cs
@@ -85,7 +85,7 @@ class Build : NukeBuild
                     .SetServer("https://sonarcloud.io")
                     .SetFramework("net5.0")
                     .SetLogin(SonarQubeApiKey)
-                    .SetProjectKey("todoist-net")
+                    .SetProjectKey("olsh_todoist-net")
                     .SetName("Todoist.Net")
                     .SetOrganization("olsh")
                     .SetVersion("1.0.0.0");
