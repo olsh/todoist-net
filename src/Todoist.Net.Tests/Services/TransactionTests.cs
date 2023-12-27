cb30a781634ae0be7e57018b9539388acab65daa
Ahmed Zaki <Ahmed.Z13@outlook.com>
2023-12-27 08:18:00 +0200

    Added transaction tests for predefined temp_id as discussed in #43

--- src/Todoist.Net.Tests/Services/TransactionTests.cs
+++ src/Todoist.Net.Tests/Services/TransactionTests.cs
@@ -44,5 +44,83 @@ namespace Todoist.Net.Tests.Services
 
             deleteTransaction.CommitAsync().Wait();
         }
+
+        [Fact]
+        [Trait(Constants.TraitName, Constants.IntegrationFreeTraitValue)]
+        public void CreateProjectAndCreateItem_Success()
+        {
+            var client = TodoistClientFactory.Create(_outputHelper);
+
+            var transaction = client.CreateTransaction();
+
+            var project = new Project("Shopping List");
+            var projectId = transaction.Project.AddAsync(project).Result;
+
+            var item = new Item("Buy milk")
+            {
+                ProjectId = projectId
+            };
+            transaction.Items.AddAsync(item).Wait();
+
+            transaction.CommitAsync().Wait();
+
+            Assert.False(string.IsNullOrEmpty(project.Id.PersistentId));
+            Assert.False(string.IsNullOrEmpty(item.Id.PersistentId));
+
+
+            var itemInfo = client.Items.GetAsync(item.Id).Result;
+
+            Assert.Equal(itemInfo.Item.Content, item.Content);
+            Assert.Equal(itemInfo.Project.Name, project.Name);
+            Assert.Equal(itemInfo.Project.Id.PersistentId, project.Id.PersistentId);
+
+
+            client.Projects.DeleteAsync(project.Id).Wait();
+
+            var projects = client.Projects.GetAsync().Result;
+
+            Assert.DoesNotContain(projects, p => p.Id.PersistentId == project.Id.PersistentId);
+        }
+
+        [Fact]
+        [Trait(Constants.TraitName, Constants.IntegrationFreeTraitValue)]
+        public void CreateProjectAndCreateItemWithPredefinedTempId_Success()
+        {
+            var client = TodoistClientFactory.Create(_outputHelper);
+
+            var project = new Project("Shopping List")
+            {
+                Id = new ComplexId(Guid.NewGuid()) // predefined temp id
+            };
+            var item = new Item("Buy milk")
+            {
+                ProjectId = project.Id // predefined temp id
+            };
+
+            var transaction = client.CreateTransaction();
+
+            transaction.Project.AddAsync(project).Wait();
+            transaction.Items.AddAsync(item).Wait();
+
+            transaction.CommitAsync().Wait();
+
+            Assert.False(string.IsNullOrEmpty(project.Id.PersistentId));
+            Assert.False(string.IsNullOrEmpty(item.Id.PersistentId));
+
+
+            var itemInfo = client.Items.GetAsync(item.Id).Result;
+
+            Assert.Equal(itemInfo.Item.Content, item.Content);
+            Assert.Equal(itemInfo.Project.Name, project.Name);
+            Assert.Equal(itemInfo.Project.Id.PersistentId, project.Id.PersistentId);
+
+
+            client.Projects.DeleteAsync(project.Id).Wait();
+
+            var projects = client.Projects.GetAsync().Result;
+
+            Assert.DoesNotContain(projects, p => p.Id.PersistentId == project.Id.PersistentId);
+        }
+
     }
 }
