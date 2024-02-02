b78a54a9570b7741409888f25b237286cc96405d
Ahmed Zaki <Ahmed.Z13@outlook.com>
2024-02-02 14:46:13 +0200

    Added methods to get collaborators to the SharingService. Similar to the methods in the recently created CollaboratorsService.

--- src/Todoist.Net/Services/ISharingService.cs
+++ src/Todoist.Net/Services/ISharingService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +12,20 @@ namespace Todoist.Net.Services
     /// </summary>
     public interface ISharingService : ISharingCommandService
     {
+        /// <summary>
+        /// Gets all collaborators.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>The collaborators.</returns>
+        /// <exception cref="HttpRequestException">API exception.</exception>
+        Task<IEnumerable<Collaborator>> GetCollaboratorsAsync(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Gets all collaborator states.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>The collaborator states.</returns>
+        /// <exception cref="HttpRequestException">API exception.</exception>
+        Task<IEnumerable<CollaboratorState>> GetCollaboratorStatesAsync(CancellationToken cancellationToken = default);
     }
 }
