2d340d01b4496a5bbc21741b9f94d0a23d877246
Ahmed Zaki <Ahmed.Z13@outlook.com>
2024-05-15 17:31:26 +0300

    Added a JsonResolver modifier to serialize properties with internal setters.

--- src/Todoist.Net/Serialization/Resolvers/JsonResolverModifiers.cs
+++ src/Todoist.Net/Serialization/Resolvers/JsonResolverModifiers.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
 
 using Todoist.Net.Models;
@@ -6,6 +9,42 @@ namespace Todoist.Net.Serialization.Resolvers
 {
     internal static class JsonResolverModifiers
     {
+        public static void SerializeInternalSetters(JsonTypeInfo typeInfo)
+        {
+            string GetPropertyName(PropertyInfo property)
+            {
+                var propertyNameAttribute = property.GetCustomAttribute<JsonPropertyNameAttribute>();
+
+                return propertyNameAttribute?.Name
+                    ?? typeInfo.Options.PropertyNamingPolicy?.ConvertName(property.Name)
+                    ?? property.Name;
+            }
+
+            var internalSetterProperties = typeInfo.Type
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(property => property.SetMethod?.IsAssembly == true);
+
+            var nullSetterJsonProperties = typeInfo.Properties
+                .Where(property => property.Set == null);
+
+            var joinedProperties = internalSetterProperties
+                .Select(property => new
+                {
+                    Property = property,
+                    Name = GetPropertyName(property)
+                })
+                .Join(nullSetterJsonProperties, x => x.Name, y => y.Name, (x, y) => new
+                {
+                    PropertyInfo = x.Property,
+                    JsonPropertyInfo = y
+                });
+
+            foreach (var property in joinedProperties)
+            {
+                property.JsonPropertyInfo.Set = property.PropertyInfo.SetValue;
+            }
+        }
+
         public static void FilterSerializationByType(JsonTypeInfo typeInfo)
         {
             if (typeInfo.Type == typeof(UserInfo))
--- src/Todoist.Net/TodoistClient.cs
+++ src/Todoist.Net/TodoistClient.cs
@@ -32,6 +32,7 @@ namespace Todoist.Net
             {
                 Modifiers =
                 {
+                    JsonResolverModifiers.SerializeInternalSetters,
                     JsonResolverModifiers.FilterSerializationByType
                 }
             },
