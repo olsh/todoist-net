24a43f029492a8a0552e36fd7269671364b2526a
Ahmed Zaki <Ahmed.Z13@outlook.com>
2024-05-31 08:22:07 +0300

    Removed `class` constraint from `Unset` method and skipped internal setters.

--- src/Todoist.Net/Extensions/UnsettablePropertiesExtensions.cs
+++ src/Todoist.Net/Extensions/UnsettablePropertiesExtensions.cs
@@ -19,15 +19,22 @@ namespace Todoist.Net
         /// <param name="entity">The entity to unset its property.</param>
         /// <param name="propertyExpression">A lambda expression representing the property to be unset (x => x.MyProperty).</param>
         /// <exception cref="ArgumentException"><paramref name="propertyExpression"/> is not a lambda for property.</exception>
-        public static void Unset<T, TProp>(this T entity, Expression<Func<T, TProp>> propertyExpression)
-            where T : IUnsettableProperties
-            where TProp : class
+        public static void Unset<T, TProp>(this T entity, Expression<Func<T, TProp>> propertyExpression) where T : IUnsettableProperties
         {
+            // We use runtime check instead of generic constraints to allow both reference and Nullable<T> types.
+            if (default(TProp) != null)
+            {
+                throw new InvalidOperationException("Property type is required to be a nullable type.");
+            }
             var propertyInfo = (propertyExpression.Body as MemberExpression)?.Member as PropertyInfo
                 ?? throw new ArgumentException($"Invalid property expression: ({propertyExpression})", nameof(propertyExpression));
 
-            // Make sure that the property is null or default.
-            propertyInfo.SetValue(entity, null);
+            // If the property doesn't have an internal setter.
+            if (!propertyInfo.SetMethod.IsAssembly)
+            {
+                // Make sure that the property is null.
+                propertyInfo.SetValue(entity, null);
+            }
 
             entity.UnsetProperties.Add(propertyInfo);
         }
