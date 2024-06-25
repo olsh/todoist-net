0838ee61866345eb4a9a463e0bc5376cee7ad4dc
Ahmed Zaki <Ahmed.Z13@outlook.com>
2024-06-25 10:45:17 +0300

    Added a `TodoistClientFactory` that builds `TodoistClients` with tokens.

---
+++ src/Todoist.Net/TodoistClientFactory.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+
+namespace Todoist.Net
+{
+    internal sealed class TodoistClientFactory : ITodoistClientFactory
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TodoistClientFactory(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        /// <inheritdoc/>
+        public TodoistClient CreateClient(string token)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var todoistRestClient = new TodoistRestClient(token, httpClient);
+
+            return new TodoistClient(todoistRestClient);
+        }
+    }
+}
