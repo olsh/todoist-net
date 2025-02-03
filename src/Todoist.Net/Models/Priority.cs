184e5f7574e2073f1e78cf3663dd43b7421e9de7
Oleg Shevchenko <olsh.me@gmail.com>
2025-02-03 15:24:16 +0300

    Update priority mapping. Fixes #63 (#65)

--- src/Todoist.Net/Models/Priority.cs
+++ src/Todoist.Net/Models/Priority.cs
@@ -3,26 +3,30 @@
     /// <summary>
     /// Priority of a task.
     /// </summary>
+    /// <remarks>
+    /// The priority of the task (a number between 1 and 4, 4 for very urgent and 1 for natural).
+    /// Note: Keep in mind that very urgent is the priority 1 on clients. So, p1 will return 4 in the API.
+    /// </remarks>
     public enum Priority : byte
     {
         /// <summary>
         /// The priority1
         /// </summary>
-        Priority1 = 1,
+        Priority1 = 4,
 
         /// <summary>
         /// The priority2
         /// </summary>
-        Priority2 = 2,
+        Priority2 = 3,
 
         /// <summary>
         /// The priority3
         /// </summary>
-        Priority3 = 3,
+        Priority3 = 2,
 
         /// <summary>
         /// The priority4
         /// </summary>
-        Priority4 = 4
+        Priority4 = 1
     }
 }
